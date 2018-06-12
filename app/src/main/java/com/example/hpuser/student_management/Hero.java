d3213966f039d4008a40536336a90bdce842139f
akshatrana22 <akshatrana2707@gmail.com>
2018-06-12 22:57:13 +0530

    my commit

---
+++ app/src/main/java/com/example/hpuser/student_management/Hero.java
@@ -0,0 +1,17 @@
+package com.example.hpuser.student_management;
+
+public class Hero {
+
+    String name;
+    long phone;
+    String college;
+    String address;
+
+    public Hero( String name,long phone,String college,String address)
+    {
+        this.name=name;
+        this.phone=phone;
+        this.college=college;
+        this.address=address;
+    }
+}
--- app/src/main/java/com/example/hpuser/student_management/MainActivity.java
+++ app/src/main/java/com/example/hpuser/student_management/MainActivity.java
@@ -3,41 +3,32 @@ package com.example.hpuser.student_management;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-class hero
-{
-    String name;
-    int phone;
-    String college;
-    String address;
-
-   public hero( String name,int phone,String college,String address)
-    {
-        this.name=name;
-        this.phone=phone;
-        this.college=college;
-        this.address=address;
-    }
-
-}
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText ob1;
     EditText ob2;
-    EditText ob3;
+    Spinner  ob3;
+    EditText ob5;
     EditText ob4;
     Button b1;
     Button b2;
     TextView t;
+    String collegename="";
 
+        String collnames[]={"select college","DIT", "GEU", "UIT"};
 
-    ArrayList<hero> al=new ArrayList<hero>();
+    ArrayList<Hero> al=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,26 +42,31 @@ public class MainActivity extends AppCompatActivity {
         b1= findViewById(R.id.button_1);
         b2= findViewById(R.id.button_2);
         t= findViewById(R.id.students);
+        ob3.setOnItemSelectedListener(this);
+
+        ArrayAdapter arrayAdapter= new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,collnames);
+        ob3.setPrompt(collnames[0]);
+        ob3.setAdapter(arrayAdapter);
+
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try{
                 String name= String.valueOf(ob1.getText());
-                String college= String.valueOf(ob3.getText());
                 String address= String.valueOf(ob4.getText());
-                int phone= Integer.parseInt(ob2.getText().toString());
+                long phone= Long.parseLong(ob2.getText().toString());
 
-                if (name.isEmpty()|| college.isEmpty()||address.isEmpty()||phone==0)
+                if (name.isEmpty()||address.isEmpty()||phone==0)
                 {
                     Toast.makeText(getApplicationContext(),"enter valid entries",Toast.LENGTH_SHORT).show();
                 }
                 else {
 
-                    hero obj= new hero(name,phone,college,address);
+                    //hero obj= new hero(name,phone,collegename,address);
 
 
-                     al.add(new hero(name, phone, college, address));
+                     al.add(new Hero(name, phone, collegename, address));
 
 
                 }
@@ -95,7 +91,8 @@ public class MainActivity extends AppCompatActivity {
                     t.setText(t.getText()+"\n"+al.get(i).name);
                     t.setText(t.getText()+"\n"+al.get(i).phone);
                     t.setText(t.getText()+"\n"+al.get(i).college);
-                    t.setText(t.getText()+"\n"+al.get(i).address);
+                    t.setText(t.getText()+"\n"+al.get(i).address+"\n"+"******");
+
 
                 }
             }
@@ -104,4 +101,49 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        collegename=collnames[position];
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
+
+
+
